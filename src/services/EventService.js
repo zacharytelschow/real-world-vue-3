44a34eeb545bcd7c44e616ea7808cfdd59b69b08
Zachary Telschow <zacharytelschow@gmail.com>
2020-12-31 13:29:48 -0500

    Using HTTPS instead of HTTP

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: 'http://my-json-server.typicode.com/zacharytelschow/real-world-vue-3',
+    baseURL: 'https://my-json-server.typicode.com/zacharytelschow/real-world-vue-3',
     withCredentials: false,
     headers: {
       Accept: 'application/json',
