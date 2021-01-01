e10c9c1fdc1c7f89de715a86751d0cf198e2bb6b
Zachary Telschow <zacharytelschow@gmail.com>
2021-01-01 12:18:12 -0500

    Add EventDetails, dynamic routing to support.

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,30 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+    <p>{{ event.description }}</p>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService.js';
+
+export default {
+  props: ["id"],
+  data() {
+    return {
+      event: null
+    }
+  },
+  created() {
+    // fetch event (by id) and set local event data
+    EventService.getEvent(this.id)
+      .then(resp => {
+        this.event = resp.data;
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+}
+</script>
